595a28b4a929e3ab9c26fae7c7c95e4581176df8
Jose Gil Lizardo <josegil9807@gmail.com>
2025-03-28 09:55:00 -0400

    feat: add pocket user to auth

--- pkg/rest/auth/pocket_user.go
+++ pkg/rest/auth/pocket_user.go
@@ -5,10 +5,11 @@ import "context"
 // PocketUser represents a user in the pocket pilot system
 // This is the user that is authenticated to the system
 type PocketUser struct {
-	UserId      string   `json:"https://pocket-pilot.com/user_id"`
-	Email       string   `json:"https://pocket-pilot.com/email"`
-	Role        []string `json:"https://pocket-pilot.com/role"`
+	UserId      string   `json:"https://pocketpilot.com/user_id"`
+	Email       string   `json:"https://pocketpilot.com/email"`
+	Roles       []string `json:"https://pocketpilot.com/roles"`
 	Permissions []string `json:"permissions"`
+	Scope       string   `json:"scope"`
 	Subject     string   `json:"sub"`
 }
 
